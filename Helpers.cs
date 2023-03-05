341516425d5e059f52edcb0c5a07e764ae5057bc
John-Stuart Gonsales <johnstuartgonsales@gmail.com>
2023-03-05 01:58:44 +0000

    added get number input function

---
+++ Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flashcards
+{
+    internal class Helpers
+    {
+        internal static int GetNumberInput()
+        {
+            string Input = Console.ReadLine();
+            int ConvertedInput;
+
+            while (!int.TryParse(Input, out ConvertedInput))
+            {
+                Console.WriteLine("Please input numbers");
+                Input = Console.ReadLine();
+            }
+            return ConvertedInput;
+        }
+    }
+}
